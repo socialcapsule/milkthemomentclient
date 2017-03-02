3180a66a2cd154ae919a895f92a3e59e1785bfb3
milkthemovement <milkthemovement@gmail.com>
2017-03-02 12:16:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,11 +12,12 @@ if (!databaseUri) {
 }
 
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+  databaseURI: databaseUri || 'mongodb://heroku_cwjf53n3:4qcofu0per1fbmpvmghb9gdav5@ds113630.mlab.com:13630/heroku_cwjf53n3',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  appId: process.env.APP_ID || 'ZswFjDIP7RWRLSOB7djX',
+  masterKey: process.env.MASTER_KEY || 'ItIlOFr6frwLH0GvFbdE', //Add your master key here. Keep it secret!
+  fileKey:process.env.FILE_KEY || 'eG6e33BFBuLbFd71VHNx', // Add your file key here.
+  serverURL: process.env.SERVER_URL || 'https://milkthemomentclient.herokuapp.com/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
